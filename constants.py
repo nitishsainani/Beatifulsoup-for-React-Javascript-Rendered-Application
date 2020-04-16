681dad19f155b6747cd5d6d2d3b3ee96533f0e6c
Nitish Sainani <nitish.sainani@gmail.com>
2020-04-16 13:53:48 +0530

    Initial Commit

---
+++ constants.py
@@ -0,0 +1,14 @@
+class Constants:
+    
+
+    ## Platform constants
+    platform_type_WINDOWS = "WINDOWS"
+    platform_type_LINUX = "LINUX"
+    platform_type_MAC = "MAC"
+
+    ## Driver Paths
+    driver_path = {
+        platform_type_WINDOWS: "",
+        platform_type_LINUX: "",
+        platform_type_MAC: "",
+    }
---
+++ selenium_driver.py
@@ -0,0 +1,23 @@
+import sys
+from constants import Constants
+from selenium import webdriver
+
+class SeleniumDriver():
+    def __init__(self):
+        platform = sys.platform
+        if platform.lower()[:3] == "win":
+            self.platform = Constants().platform_type_WINDOWS
+        elif platform.lower()[:3] == "lin":
+            self.platform = Constants().platform_type_LINUX
+        elif platform.lower()[:3] == "dar":
+            self.platform = Constants().platform_type_MAC
+        else:
+            raise Exception("Platform Not Supported!")
+    
+    def get_driver(self):
+        constants = Constants()
+        if self.platform == constants.platform_type_MAC:
+            return webdriver.Firefox(constants.driver_path[constants.platform_type_MAC])
+        elif self.platform == constants.platform_type_WINDOWS:
+            return webdriver.Firefox(constants.driver_path[constants.platform_type_WINDOWS])
+        elif self.platform == constants.platform_type_LINUX
